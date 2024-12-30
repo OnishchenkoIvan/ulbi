d622b8ce13d2693a27a2d36a022d73947ad4d225
IOnishchenko <iv.onishchenko@mail.ru>
2024-12-30 22:33:31 +0300

    2 Router, App Layout, i18n, eslint, stylelint, jest 15 i18n Интернационализация. Define plugin. Плагин для переводов

---
+++ src/widgets/LangSwitcher/LangSwitcher.tsx
@@ -0,0 +1,23 @@
+import {useTranslation} from "react-i18next";
+import {Button, ThemeButton} from "shared/ui/Button/Button";
+import {classNames} from "shared/lib/classNames/classNames";
+import cls from "./LangSwitcher.module.scss";
+
+interface LangSwitcherProps {
+    className?: string;
+}
+
+export const LangSwitcher = ({className}: LangSwitcherProps) => {
+    const {t, i18n} = useTranslation();
+
+    const toggleLanguage = async () => {
+        i18n.changeLanguage(i18n.language === 'en' ? 'ru' : 'en');
+    }
+
+    return (<Button
+        className={classNames(cls.LangSwitcher, {}, [className])}
+        theme={ThemeButton.CLEAR}
+        onClick={toggleLanguage}>
+        {t('Язык')}
+    </Button>)
+};
