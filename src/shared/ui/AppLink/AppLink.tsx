acb1ec38e90f504a5b9dd3330a0c160a5fdf51b2
IOnishchenko <iv.onishchenko@mail.ru>
2024-12-31 22:12:02 +0300

    2 Router, App Layout, i18n, eslint, stylelint, jest 18 Настраиваем EsLint. Исправляем ошибки

--- src/shared/ui/AppLink/AppLink.tsx
+++ src/shared/ui/AppLink/AppLink.tsx
@@ -1,7 +1,7 @@
-import {classNames} from "shared/lib/classNames/classNames";
-import cls from "./AppLink.module.scss";
-import {Link, LinkProps} from "react-router-dom";
-import {FC} from "react";
+import { classNames } from 'shared/lib/classNames/classNames';
+import { Link, LinkProps } from 'react-router-dom';
+import { FC } from 'react';
+import cls from './AppLink.module.scss';
 
 export enum AppLinkTheme {
     PRIMARY = 'primary',
@@ -14,11 +14,13 @@ interface AppLinkProps extends LinkProps {
 }
 
 export const AppLink: FC<AppLinkProps> = (props) => {
-    const {className, children, theme = AppLinkTheme.PRIMARY, to, ...otherProps} = props;
+  const {
+    className, children, theme = AppLinkTheme.PRIMARY, to, ...otherProps
+  } = props;
 
-    return (
-        <Link to={to} className={classNames(cls.AppLink, {}, [className, cls[theme]])} {...otherProps}>
-            {children}
-        </Link>
-    );
+  return (
+    <Link to={to} className={classNames(cls.AppLink, {}, [className, cls[theme]])} {...otherProps}>
+      {children}
+    </Link>
+  );
 };
--- src/widgets/LangSwitcher/LangSwitcher.tsx
+++ src/widgets/LangSwitcher/LangSwitcher.tsx
@@ -1,23 +1,26 @@
-import {useTranslation} from "react-i18next";
-import {Button, ThemeButton} from "shared/ui/Button/Button";
-import {classNames} from "shared/lib/classNames/classNames";
-import cls from "./LangSwitcher.module.scss";
+import { useTranslation } from 'react-i18next';
+import { Button, ThemeButton } from 'shared/ui/Button/Button';
+import { classNames } from 'shared/lib/classNames/classNames';
+import cls from './LangSwitcher.module.scss';
 
 interface LangSwitcherProps {
     className?: string;
 }
 
-export const LangSwitcher = ({className}: LangSwitcherProps) => {
-    const {t, i18n} = useTranslation();
+export const LangSwitcher = ({ className }: LangSwitcherProps) => {
+  const { t, i18n } = useTranslation();
 
-    const toggleLanguage = async () => {
-        i18n.changeLanguage(i18n.language === 'en' ? 'ru' : 'en');
-    }
+  const toggleLanguage = async () => {
+    i18n.changeLanguage(i18n.language === 'en' ? 'ru' : 'en');
+  };
 
-    return (<Button
-        className={classNames(cls.LangSwitcher, {}, [className])}
-        theme={ThemeButton.CLEAR}
-        onClick={toggleLanguage}>
-        {t('Язык')}
-    </Button>)
+  return (
+    <Button
+      className={classNames(cls.LangSwitcher, {}, [className])}
+      theme={ThemeButton.CLEAR}
+      onClick={toggleLanguage}
+    >
+      {t('Язык')}
+    </Button>
+  );
 };
