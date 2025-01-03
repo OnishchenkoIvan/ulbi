14c4b2b91f8b91b07615ecbe652d8dae837cd297
IOnishchenko <iv.onishchenko@mail.ru>
2025-01-03 20:35:33 +0300

    3 24 React Testing Library. Тесты на компоненты метка

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -1,9 +1,17 @@
-export default {
+const path = require('path');
+
+module.exports = {
   coverageDirectory: 'coverage',
   testEnvironment: 'jsdom',
   coveragePathIgnorePatterns: ['/node_modules/', '/dist/'],
+  rootDir: '../../',
   moduleDirectories: ['node_modules'],
   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json', 'node'],
-  rootDir: '../../',
+  modulePaths: ['<rootDir>src'],
   testMatch: ['<rootDir>/src/**/*(*.)@(spec|test).[tj]s?(x)'],
+  moduleNameMapper: {
+    '\\.s?css$': 'identity-obj-proxy',
+    '\\.svg': path.resolve(__dirname, 'jestEmptyComponent.tsx'),
+  },
+  setupFilesAfterEnv: ['<rootDir>config/jest/setupTests.ts'],
 };
